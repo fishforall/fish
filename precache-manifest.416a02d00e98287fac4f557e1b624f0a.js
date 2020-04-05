a49f68dba6a2ce473781409080ba2f529a33ff95
Konrad <konradmleczko85@gmail.com>
2020-04-05 21:54:32 +0200

    Updates

---
+++ precache-manifest.416a02d00e98287fac4f557e1b624f0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1865c9cb428f4284043d604dae945e",
+    "url": "/fish/index.html"
+  },
+  {
+    "revision": "83a3b76ba5ef56317f35",
+    "url": "/fish/static/css/main.4bcc1e3b.chunk.css"
+  },
+  {
+    "revision": "8a8fca6292ca21343ba5",
+    "url": "/fish/static/js/2.477c6c43.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/fish/static/js/2.477c6c43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a3b76ba5ef56317f35",
+    "url": "/fish/static/js/main.2f512f43.chunk.js"
+  },
+  {
+    "revision": "55967c40af6568a4737e",
+    "url": "/fish/static/js/runtime-main.0ecfbafd.js"
+  }
+]);
